a2a1c3cec03e034c0002c996aaa27380f06ff0b4
elfayyedh <elfayyedh17@gmail.com>
2023-07-24 15:54:10 +0700

    add

---
+++ rental_mobil.sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jul 2023 pada 11.31
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `booking`
+--
+
+CREATE TABLE `booking` (
+  `id_booking` int(11) NOT NULL,
+  `kode_booking` varchar(255) NOT NULL,
+  `id_login` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
+  `ktp` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_tlp` varchar(15) NOT NULL,
+  `tanggal` varchar(255) NOT NULL,
+  `lama_sewa` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `konfirmasi_pembayaran` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `booking`
+--
+
+INSERT INTO `booking` (`id_booking`, `kode_booking`, `id_login`, `id_mobil`, `ktp`, `nama`, `alamat`, `no_tlp`, `tanggal`, `lama_sewa`, `total_harga`, `konfirmasi_pembayaran`, `tgl_input`) VALUES
+(6, '1687765662', 4, 6, '123', 'fler', 'janti', 'xxe', '2023-06-23', 2, 1000879, 'Sedang di proses', '2023-06-26'),
+(7, '1687766469', 4, 6, 'wd', 'MUH. RAFFI MAULANA', 'ee', 'w3wd', '2023-06-01', 2, 1000794, 'Belum Bayar', '2023-06-26'),
+(8, '1687770634', 4, 6, '321', 'fer', 'banut', '334', '6778-03-04', 2, 1000520, 'Sedang di proses', '2023-06-26'),
+(9, '1688531580', 4, 8, '23456', 'zaki', 'bantul', '08234575322', '2023-07-06', 2, 1200726, 'Pembayaran di terima', '2023-07-05');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `infoweb`
+--
+
+CREATE TABLE `infoweb` (
+  `id` int(11) NOT NULL,
+  `nama_rental` varchar(255) DEFAULT NULL,
+  `telp` varchar(15) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `no_rek` text DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `infoweb`
+--
+
+INSERT INTO `infoweb` (`id`, `nama_rental`, `telp`, `alamat`, `email`, `no_rek`, `updated_at`) VALUES
+(1, 'Rental UAD', '085163656781', 'Seturan City and Barbasari', 'rentaluad@gmail.com', 'BRI A/N Raffi 123123213123', '2022-01-24 04:57:29');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `nama_pengguna` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id_login`, `nama_pengguna`, `username`, `password`, `level`) VALUES
+(1, 'UAD', 'admin', 'fe01ce2a7fbac8fafaed7c982a04e229', 'admin'),
+(3, 'Krisna Waskita', 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', 'pengguna'),
+(4, 'fer', 'fer', '90eb8760c187a2097884ed4c9ffbb6a4', 'pengguna');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` int(11) NOT NULL,
+  `no_plat` varchar(255) NOT NULL,
+  `merk` varchar(255) NOT NULL,
+  `harga` int(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `no_plat`, `merk`, `harga`, `deskripsi`, `status`, `gambar`) VALUES
+(5, 'AB 34234 JFH', 'Avanza', 200000, 'Avanza Tahun 2012', 'Tidak Tersedia', '1673593078toyota-all-new-avanza-2015-tangkapan-layar_169.jpeg'),
+(6, 'AB 1232 BKT', 'New Xenia', 500000, 'New Xenia Tahun 2019', 'Tersedia', 'all-new-xenia-exterior-tampak-perspektif-depan---varian-1.5r-ads.jpg'),
+(7, 'AB 321 UHJ', 'Ertiga', 200000, 'Ertiga Tahun 2017', 'Tidak Tersedia', '1688530180.jpg'),
+(8, 'AB 3424 KKL', 'Civic', 600000, 'Civic tahun 2018', 'Tersedia', '1688530196.jpg'),
+(9, 'AB 1819 JKL', 'Inova', 450000, 'Inova Tahun 2016', 'Tersedia', '1688532066.jpg'),
+(10, 'AB 3587 LKL', 'Ayla', 150000, 'Ayla Tahun 2017', 'Tersedia', '1689085782.jpg'),
+(11, 'AA 1321 PMN', 'Alphard', 1000000, 'Alphard Tahun 2016', 'Tersedia', '1689130199.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_booking` int(255) NOT NULL,
+  `no_rekening` int(255) NOT NULL,
+  `nama_rekening` varchar(255) NOT NULL,
+  `nominal` int(255) NOT NULL,
+  `tanggal` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_booking`, `no_rekening`, `nama_rekening`, `nominal`, `tanggal`) VALUES
+(3, 1, 2131241, 'Krisna Aldi Waskito', 400000, '2019-12-14'),
+(4, 2, 2131241, 'Krisna Aldi Waskito', 400525, '2019-12-18'),
+(5, 3, 13213, 'Fauzan Falah', 800743, '2022-01-24'),
+(6, 5, 0, 'MUH. RAFFI MAULANA', 500, '2023-06-06'),
+(7, 6, 34, 'MUH. RAFFI MAULANA', 1, '2023-06-30'),
+(8, 8, 344, 'fer', 1, '7778-02-14'),
+(9, 9, 123456752, 'rea', 1, '2023-07-05');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_pengembalian` int(11) NOT NULL,
+  `kode_booking` varchar(255) NOT NULL,
+  `tanggal` varchar(255) NOT NULL,
+  `denda` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id_booking`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
+-- Indeks untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
